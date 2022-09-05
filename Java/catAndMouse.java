a2c37eada3de687d0b97cc56158194b6125caf10
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-09-05 12:41:48 +0100

    Update

--- Java/catAndMouse.java
+++ Java/catAndMouse.java
@@ -2,12 +2,12 @@ import java.lang.Math;
 
 class catAndMouse{
     public static void main(String[] args) {
-        int catM[] = [1, 3, 2];
-        System.out.print(catMouse(catM));
+        int catM[] = { 1, 3, 2 };
+        System.out.print(catMouse(catM[0], catM[1], catM[2]));
     }
-    
-    public int catMouse(int cat1, int cat2, int mouse ) {
-       if (math.lang.Math.abs(cat1 - mouse) == math.lang.Math.abs(cat2 - mouse)) {
+
+    public static String catMouse(int cat1, int cat2, int mouse ) {
+       if (Math.abs(cat1 - mouse) == Math.abs(cat2 - mouse)) {
             return "Mouse C";
         } else if(java.lang.Math.abs(cat1 - mouse) < java.lang.Math.abs(cat2 - mouse)) {
             return "Cat A";
