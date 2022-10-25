d0ae37acea51d13c8a1ab21cf080610d2788f88c
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-10-25 15:35:59 +0100

    Updated BinaryTree

--- Python/BinaryTree.py
+++ Python/BinaryTree.py
@@ -1,4 +1,4 @@
-class Node:
+class BinaryTree(object):
     # Initiating 
     def __init__(self, data):
         self.left = None
@@ -9,16 +9,29 @@ class Node:
         if self.data:
             if data < self.data:
                 if self.left is None:
-                    self.left = Node(data)
+                    self.left = BinaryTree(data)
                 else:
                     self.left.insert(data)
             elif data > self.data:
                 if self.right is None:
-                    self.right = Node(data)
+                    self.right = BinaryTree(data)
                 else:
                     self.right.insert(data)
         else:
             self.data = data
+    # Find a value in tree
+    def find(self, data, count=0):
+        count += 1
+        if data < self.data:
+            if self.left is None:
+                return f"{data} is not in the Tree"
+            return self.left.find(data, count=count)
+        elif data > self.data:
+            if self.right is None:
+                return f"{data} is not in the Tree"
+            return self.right.find(data, count=count)
+        return f"{data} is in the Tree and it is on level {count}"
+
     # Print the Tree
     def PrintTree(self):
         if self.left:
@@ -26,6 +39,7 @@ class Node:
         print(self.data)
         if self.right:
             self.right.PrintTree()
+
     # Inorder traversal
     # Left -> Root -> Right
     def inorderTraversal(self, root):
@@ -45,14 +59,16 @@ class Node:
             return []
         return self.postorderTraversal(root.left)  + self.postorderTraversal(root.right) + [root.data]
 
-root = Node(27)
+root = BinaryTree(27)
 root.insert(14)
 root.insert(35)
 root.insert(10)
 root.insert(19)
 root.insert(31)
 root.insert(42)
+root.insert(45)
 root.PrintTree()
+print(root.find(45))
 print(root.inorderTraversal(root))
 print(root.preorderTraversal(root))
 print(root.postorderTraversal(root))
